4e41da3c16a02df1b397d38ad7b6e20d5aed19b4
Mihir Dabgar <mihir@heliconia.in>
2024-11-20 12:07:25 +0530

    [IMP] Improved name and view string changes

--- hspl_saas_config/models/saas.py
+++ hspl_saas_config/models/saas.py
@@ -4,13 +4,13 @@ from odoo import api, fields, models
 class SaasTemplate(models.Model):
     _inherit = "saas.template"
 
-    expiry_notify_days = fields.Integer(string="Expiry Days", default=3)
+    expiry_notify_days = fields.Integer(string="Pre Expiry Notification", default=3)
 
 
 class SaasDB(models.Model):
     _inherit = "saas.db"
 
-    expiry_notify_days = fields.Integer(string="Expiry Days")
+    expiry_notify_days = fields.Integer(string="Pre Expiry Notification")
 
     def write(self, vals):
         res = super(SaasDB, self).write(vals)
@@ -23,13 +23,13 @@ class SaasDB(models.Model):
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    expiry_notify_days = fields.Integer(string="Expiry Days", store=True, related='saas_package_id.expiry_notify_days')
+    expiry_notify_days = fields.Integer(string="Pre Expiry Notification", store=True, related='saas_package_id.expiry_notify_days')
 
 
 class ContractContract(models.Model):
     _inherit = "contract.contract"
 
-    expiry_notify_days = fields.Integer(related="build_id.expiry_notify_days")
+    expiry_notify_days = fields.Integer(related="build_id.expiry_notify_days", string="Pre Expiry Notification")
 
     @api.model_create_multi
     def create(self, vals_list):
